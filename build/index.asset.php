f2f4dc2db8fed00c0a47233602bd17272674c7f0
imath <math.viet@gmail.com>
2025-07-09 11:37:20 +0200

    Build the block

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '960dea10f4e260448a4e');
