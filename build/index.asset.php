8d9b3d10fa5ef06ba03a938ffa2f244ca54c8262
imath <math.viet@gmail.com>
2026-07-24 00:29:50 +0200

    Make sure Gists are better styled
    Some important CSS variables were no more defined by GitHub, this commit bring them back inside the block's main CSS file.
    
    Fixes #4

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4ec5baa90496225acad9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2eea55f4bd66ab0f0bff');
